9bc04876363527a9cbdeac2d320e73cabc7bedc8
vangampalli <87544357+vangampalli@users.noreply.github.com>
2022-03-13 01:57:32 -0500

    push

--- static/js/app.js
+++ static/js/app.js
@@ -1,91 +1,219 @@
-async function getBellyButtonData() {
-    var response = await fetch("./samples.json"); 
-    var data = await response.json();
-    console.log(data);
+var names = []
+var metadata = []
+var sample = []
 
-    //making arrays for each element, sorting by the top 10, and then slicing those 10 from the rest of the data
-    //sample at given index---filtered for top 10 otu
-    var samps = Object.values(data.samples[0]).sort((firstNum, secondNum) => secondNum.sample_values - firstNum.sample_values);
-    console.log(samps);
-
-    //creating a list of ids 
-    var idlist = Object.values(data.names)
-    console.log(idlist);
-
-    //sample id 
-    var samplenum = samps[0]
-    console.log(samplenum)
-
-    //otu ids 
-    var otu_ids = Object.values(samps[1]).slice(0, 10);
-    console.log(otu_ids)
-
-    //sample values 
-    var sample_values = Object.values(samps[2]).slice(0, 10);
-    console.log(sample_values);
-
-    //otu labels 
-    var labels = Object.values(samps[3]).slice(0, 10);
-    console.log(labels)
-
-    //setting up bar chart 
-    let trace1 = {
-        x: sample_values, 
-        y: otu_ids.toString(),
-        type: "bar",
-        orientation: 'h'
-    };
-
-    let traceData = [trace1];
-
-    let layout = {
-        title: "Otu_IDS",
-      };
-
-    Plotly.newPlot("bar", traceData, layout);
-
-    //populating dropdown menu
-    
-    for (var i = 0; i < idlist.length;  i++) {
-        var currentOption = document.createElement('option');
-        currentOption.textContent = data.samples[i];
-        document.querySelector("selDataset").append(currentOption)
-    }
+var otu_id_list = []
+var sample_value = []
+var otu_label = []
 
 
-    //adding event listener 
+
+async function main() {
+    //get data and put variables to work with later
+    const response = await fetch("./samples.json");
+    data = await response.json();
+    for (var i=0; i<153; i++) {
+        names.push(data.names[i])
+        metadata.push(data.metadata[i])
+        sample.push(data.samples[i])
+        otu_id_list.push(sample[i].otu_ids)
+        otu_label.push(sample[i].otu_labels)
+        sample_value.push(sample[i].sample_values)
+    };  
+
+    console.log(names.length)
+    console.log(metadata)
+    console.log(sample) 
+    console.log(otu_id_list)
+    console.log(otu_label)
+    console.log(sample_value)
+    console.log(names[0])
+
+    //add event listener to change the webpage when selecting an option
+    document.querySelector("#selDataset").value = names;
 
     document.querySelector("#selDataset").addEventListener("change", event => {
-        let samples = []
+        metadata_fn(event.target.value);
+        charts(event.target.value);
+      })
 
-        for (let i = 0; i <samplenum.length; i++) {
-            if (event.target.value === i);
-            samples = data.samples[i];
+    for (var i=0; i<153; i++) {
+        var currentOption = document.createElement("option");
+        currentOption.text = names[i];
+        document.querySelector("select").append(currentOption);
+    };
 
-        }
-    })
+
+    metadata_fn(names[0]);
+    charts(names[0]);
+
 };
 
 
-getBellyButtonData();
+async function metadata_fn(sample) {
+    
+    dem_info = data.metadata.filter(col => col.id == sample)[0];
+    
+    console.log(dem_info);
 
+    panel = document.querySelector("#sample-metadata");
 
+    panel.innerHTML = ''; 
 
-//for (let i = 0; i < stooges.length; i++) {
-//     console.log(stooges[i]);
-// }
+    Object.entries(dem_info).forEach(([_key, _value]) => {
+        const h6 = document.createElement("h6");
+        h6.textContent = _key + ':' + _value
+        panel.append(h6);
+    });
+};
 
+async function charts(sample) {
 
+    bub_data = data.samples.filter(col => col.id == sample)[0];
 
+    var bubble_chart = { 
+        title: 'Bacteria Cultures Per Sample',
+        margin: { t:0 },
+        hovermade: "closest",
+        xaxis: { title: "OTU_IDS"},
+        margin: { t:30 }
+    };
 
-//create function 
-    //make arrays and put into variables 
-        //pull with fetch 
-        //create an array for x values (Top 10 OTU_IDS)
-        //create an array for y values (Sample Values)
-        //create a labels array (OTU_labels)
-    //put arrays into chart structure....
-        //code that can be made into bar chart 
-        //html plot.ly chart that has the code in it that we are looking for 
-        //
-    //relate charts to html
+    var bubbleData = [
+        {
+          x: bub_data.otu_ids,
+          y: bub_data.sample_values,
+          text: bub_data.otu_labels,
+          mode: "markers",
+          marker: {
+            size: bub_data.sample_values,
+            color: bub_data.otu_ids,
+            colorscale: "Earth"
+          }
+        }
+      ];
+
+      Plotly.newPlot("bubble", bubbleData, bubble_chart);
+
+      for (var i=0; i<153; i++) {
+            if (sample === names[i]) {
+            dependent = sample_value[i];
+            independent  = otu_id_list[i]
+            words = otu_label[i]}
+            num_otu = otu_id_list[i]
+            sample_count = sample_value[i]
+      };
+  
+      var trace1 = {
+        x: dependent.sort((secondNum, firstNum) => firstNum - secondNum).slice(0,10),
+        y: independent.toString(),
+        type: "bar",
+        text: words, 
+        orientation: 'h'
+        };
+
+      var bar_data = [trace1];
+
+      var layout = {title:'Top 10 OTUs per Subject'};
+
+      Plotly.newPlot('bar', bar_data, layout);
+
+};
+    
+
+//function for plotting graphs based on decision made above
+
+// function graph_vals(sample) {for (var i=0; i<153; i++) {
+//     var currentOption = document.createElement("option");
+//     currentOption.text = names[i];
+//     document.querySelector("select").append(currentOption);
+
+// //bar graph
+//     for (var i=0; i<153; i++) {
+//         if (sample === names[i]) {
+//         dependent = sample_value[i];
+//         independent  = otu_id_list[i]
+//         words = otu_label[i]}
+//         num_otu = otu_id_list[i]
+//         sample_count = sample_value[i]
+
+//     };
+
+//     var trace1 = {
+//         x: dependent.sort((secondNum, firstNum) => firstNum - secondNum).slice(0,10),
+//         y: independent.toString(),
+//         type: "bar",
+//         text: words, 
+//         orientation: 'h'
+//     };
+
+//     var bar_data = [trace1];
+
+//     var layout = {title:'Top 10 OTUs per Subject'};
+
+//     Plotly.newPlot('bar', bar_data, layout);
+
+// //bubble graph
+//     var trace2 = {
+//         x: independent, 
+//         y: dependent, 
+//         text: words,
+//         mode: 'markers',
+//         marker: {
+//             size: dependent,
+//             color: independent,
+//             colorscale: "Earth"
+//           }
+//     }; 
+
+//     var scat_data = [trace2]; 
+    
+//     var layout2 = {title: 'OTU IDs'}
+
+//     Plotly.newPlot('bubble', scat_data, layout2)
+// };
+data = {};
+main();
+// async function dropdown() {main()} {
+//     for (var i=0; i<153; i++) {
+//         var currentOption = document.createElement("option");
+//         currentOption.text = names[i];
+//         document.querySelector("select").append(currentOption);
+//     };
+// };
+
+// dropdown();
+
+// function setPlot(chosen_OTU) {
+//     get_sample_data(chosen_OTU); 
+
+//     var trace1 = {
+//         x: sample_values, 
+//         y: otu_ids.toString(),
+//         type: "bar",
+//         text: otu_labels, 
+//         orientation: 'h'
+//     };
+
+//     var bar_data = [trace1];
+
+//     var layout = {title:'Top 10 OTUs'};
+
+//     Plotly.newPlot('bar', bar_data, layout);
+// };
+
+// // function assignOptions(textArray, selector) {
+// //     for (var i = 0; i< textArray.length; i++) {
+// //         var currentOption = document.createElement('option');
+// //         currentOption.text = textArray[i];
+// //         selector.appendChild(currentOption);
+// //     };
+// // };
+
+// // assignOptions(otu_ids, IDselector);
+
+// // function updateOTUIDS(){
+// //     setPlot(IDselector.value);
+// // }
+
+// // updataeOTUIDS();
